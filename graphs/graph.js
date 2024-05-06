aea2c7d07196d9a74eb04383bcdb1736dbf68a3e
Johnathan Osorno <jnosornov@gmail.com>
2024-05-06 08:32:01 -0500

    graphs - removing a vertex

--- graphs/graph.js
+++ graphs/graph.js
@@ -17,4 +17,11 @@ class Graph {
     this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2)
     this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1)
   }
+  removeVertex(vertex) {
+    while(this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop()
+      this.removeEdge(vertex, adjacentVertex)
+    }
+    delete this.adjacencyList[vertex]
+  }
 }
