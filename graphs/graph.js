c63876901ddbf8d6d84bc298c15b0fe0e97b967e
Johnathan Osorno <jnosornov@gmail.com>
2024-05-06 08:18:03 -0500

    graphs - adding an edge

--- graphs/graph.js
+++ graphs/graph.js
@@ -9,4 +9,8 @@ class Graph {
     if (this.adjacencyList[vertex]) return
     this.adjacencyList[vertex] = []
   }
+  addEdge(v1,v2) {
+    this.adjacencyList[v1].push(v2)
+    this.adjacencyList[v2].push(v1)
+  }
 }
